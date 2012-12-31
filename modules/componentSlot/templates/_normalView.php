695222b60b242498c636dacc65fa1a8621cc4ba8
Scott Meves <scott@stereointeractive.com>
2012-12-31 13:09:53 -0500

    updated readme. passing slot to simpleEditButton

--- modules/componentSlot/templates/_normalView.php
+++ modules/componentSlot/templates/_normalView.php
@@ -1,4 +1,4 @@
-<?php include_partial('a/simpleEditButton', array('name' => $name, 'pageid' => $pageid, 'permid' => $permid)) ?>
+<?php include_partial('a/simpleEditButton', array('name' => $name, 'pageid' => $pageid, 'permid' => $permid, 'slot' => $slot)) ?>
 <?php if ($module):     
 ?>
   <?php include_component($module, $component, $values['params']) ?>
